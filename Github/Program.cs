0369fa40ab2b39dbbdd61b1f45e01f19cfde8e1a
Mosaj <Mosaj@KURDEN>
2023-09-11 10:27:48 +0200

    jag har lagt 2 rader

--- Github/Program.cs
+++ Github/Program.cs
@@ -6,6 +6,8 @@
         {
             Console.WriteLine("Hej från Mosa");
             Console.WriteLine("Detta är version2");
+            Console.WriteLine("rad 3");
+            Console.WriteLine("Rad 4");
         }
     }
 }
