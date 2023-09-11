47046dd4a699c64d1b19feabba9cc86a037175dd
Mosaj <Mosaj@KURDEN>
2023-09-11 11:01:48 +0200

    enum metod

--- Github/Program.cs
+++ Github/Program.cs
@@ -1,13 +1,73 @@
 ﻿namespace Github
 {
+
+    enum Beverage
+    {
+        
+        Kaffe,
+        Te,
+        Vatten,
+        Redbull,
+        Juice,
+    }
+
     internal class Program
     {
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hej från Mosa");
-            Console.WriteLine("Detta är version2");
-            Console.WriteLine("rad 3");
-            Console.WriteLine("Rad 4");
+            Console.WriteLine("Välkommen till maskinen");
+            Console.WriteLine("Vilken beverage föredrar du? (Kaffe, Te, Vatten, redbull, Juice)");
+            string userinput = Console.ReadLine();
+
+            switch (userinput.ToLower()) 
+            {
+                case "kaffe":
+                    respond(Beverage.Kaffe);
+                    break;
+                case "te":
+                    respond(Beverage.Te);
+                    break;
+                case "vatten":
+                    respond(Beverage.Vatten);
+                    break;
+                case "redbull":
+                    respond(Beverage.Redbull);
+                    break;
+                case "juice":
+                    respond(Beverage.Juice);
+                    break;
+                default: 
+                    Console.WriteLine("Fel knapp, denna dryck finns inte");
+                    break;
+
+            }
+        }
+
+        static void respond(Beverage dryck)
+        {
+            switch (dryck)
+            {
+                case Beverage.Kaffe:
+                    Console.WriteLine("Du valde Kaffe latte");
+                    break;
+                case Beverage.Te:
+                    Console.WriteLine("Du valde Te");
+                    break;
+                case Beverage.Vatten:
+                    Console.WriteLine("Du valde Vatten");
+                    break;
+                case Beverage.Redbull:
+                    Console.WriteLine("Du valde Redbull");
+                    break;
+                case Beverage.Juice:
+                    Console.WriteLine("Du valde Juice");
+                    break;
+                default:
+                    Console.WriteLine("Orange Juice kommer nu");
+                    break;
+            }
         }
     }
 }
